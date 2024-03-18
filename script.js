16b02a5ee585863a322a766848cafc5b5e8112b5
LucianaGuiduce <lucianaguiduce@hotmail.com>
2024-03-17 17:58:24 -0700

    segundo commit

--- script.js
+++ script.js
@@ -1,74 +1,85 @@
-//Inicio de bienvenida
+//Inicio de bienvenida pido que escriba si o sino te lo perdiste adios. Extra
 
-//let entrada = prompt ("Estas seguro de querer entrar a la mejor web del mundo? ")
+let entrada = prompt ("Estas seguro de querer entrar a la mejor web del mundo? ")
 
+let nombre = "si"
 
-//let nombre = "si"
-
- //   if(entrada == nombre){
-    //      alert("Bienvenido a la web de Albert Hammond Jr")
- //   }
-  //  else{
- //       alert("Te lo perdiste. Adios")
-   // }
-
-//
-
-const entradas = []
-
-let comprar = confirm("¿Queres estar en el próximo recital de Albert Hammond Jr?")
-
-if(comprar == true){
-    let entradaAcomprar = prompt("¿Queres comprar entrada vip, entrada campo o entrada meet and greet?")
-
-    while(comprar){
-        switch(entradaAcomprar){
-            case "entrada vip":
-                entradas.push("entrada vip")
-
-                break
-            case "entrada campo":
-                entradas.push("entrada campo")
-
-                break
-            case "entrada meet and greet":
-                entradas.push("entrada meet and greet")
-
-                break
-            default:
-                alert("No tenemos otro tipo de entrada disponible a la venta")
-
-        }
-        comprar = confirm("¿Quiere seguir comprardo?")
-        if(comprar){
-            entradaAcomprar = prompt("¿Queres comprar entrada vip, entrada campo o entrada meet and greet?")
-        }
-        comprar = cancel( "Hasta el próximo recital de Albert, adiós!")
-        if(comprar){
-            alert ("Bye!")
-        }
+   if(entrada == nombre){
+          alert("Bienvenido a la web de Albert Hammond Jr")
+   }
+    else{
+        alert("Te lo perdiste. Adios")
     }
 
-}else{
-    alert("Hasta el próximo recital de Albert, adiós!")
-}
+    //Funciones
+    function valida_entrada(elegida)
+    {
+        let pos = -1;
+        switch( elegida )
+        {
+            case "campo":
+                pos = 0;
+            break;
+            
+            case "platea":
+                pos = 1;
+            break;
+            
+            case "VIP":
+                pos = 2;
+            break;
+            
+            default:
+                pos = -1;
+            break;
+        }
+    
+        return pos;
+    }
+    
+    function aviso(tipo)
+    {
+        if(tipo == 1)
+        {
+            alert("Compraste la entrada super bien!");
+        }
+        else if(tipo == 0)
+        {
+            alert("No existe esa entrada, solo campo, platea y VIP");
+        }
+    }
+    
+    /*
+     Array
+    */
+    let entradasElegidas   = [] // new Array();
+    let entradasExistentes = ["Entrada Campo","Entrada Platea","Entrada VIP"];
+    
+    let confirma = confirm("Desea comprar una entrada?");
+    while(confirma == true)
+    {
+        // prompt pide que escriba, lo guardo en elegida
+        let elegida = prompt('Que entrada quiere: ' + entradasExistentes.join(","));
+    
+        // le envío elegida a la funcion valida_entrada
+        let resultado = valida_entrada(elegida);  // 0 1 2 -1;
+    
+        if( resultado != -1 )
+        {
+            entradasElegidas.push( entradasExistentes[resultado] );
+            aviso(1);
+        }
+        else
+        {
+            aviso(0);
+        }
+    
+        confirma = confirm("Desea comprar otra entrada?");	// si o no
+    }
+    
+    alert( entradasElegidas.join(",") );
 
-function felicitar(parametro1,parametro2,parametro3){
-    console.log ("Felicidades por tu compra" + "parametro1" + "parametro2" + "parametro3");
-
-}
-
-let parametro1 = "entrada campo"
-let parametro2= "entrada vip"
-let parametro3= "entrada meet and greet"
-
-felicitar();
-
-
-
-
-
-
+//alert me devuelve las entradas que seleccionó
 
     
 
